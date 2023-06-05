5c87c782e9be3bac8a1c2c6a453418de650c7f34
Stonerunner24 <eli.coyle1@gmail.com>
2023-06-05 19:31:52 -0400

    Welcome sign added

--- carlisle_coc_directory/lib/main.dart
+++ carlisle_coc_directory/lib/main.dart
@@ -57,25 +57,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Container(
       constraints: const BoxConstraints.expand(),
       decoration: const BoxDecoration(
@@ -87,7 +72,22 @@ class _MyHomePageState extends State<MyHomePage> {
       child: const Scaffold(
         backgroundColor: Colors.transparent,
         body: Column(
-          
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(35.0),
+              child: Text(
+                'WELCOME',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Poppins',
+                  fontSize: 50
+                ),
+              )
+            ),
+            
+          ]
         ),
       ),
       
