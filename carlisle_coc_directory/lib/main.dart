aac611f8bf2f98aae8a30894e9e99790831234bd
Stonerunner24 <eli.coyle1@gmail.com>
2023-07-05 10:23:28 -0400

    Pushed the login button to the bottom of the screen. Reorganized the hierarchy for more clarity.

--- carlisle_coc_directory/lib/main.dart
+++ carlisle_coc_directory/lib/main.dart
@@ -13,21 +13,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -38,18 +23,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -57,26 +31,30 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints.expand(),
-      decoration: const BoxDecoration(
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
           image: DecorationImage(
             fit: BoxFit.cover,
             image: AssetImage("assets/images/Home.png"),
           )
         ),
-      child: Container(
-        margin: const EdgeInsets.only(
-          left: 32.0,
-          right: 32.0
+        child: Container(
+          margin: const EdgeInsets.only(
+            left: 32.0,
+            right: 32.0,
           ),
-        child: const Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Column(
+          child: const Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.all(48.0),
+                padding: EdgeInsets.only(
+                  top: 48.0,
+                  left: 48.0,
+                  right: 48.0,
+                  bottom: 618.0,
+                ),
                 child: Text(
                   'WELCOME',
                   textAlign: TextAlign.center,
@@ -87,7 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
               ),
-              
               ElevatedButton(
                 style: ButtonStyle(
                   backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
@@ -97,20 +74,20 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   padding: MaterialStatePropertyAll<EdgeInsets>(EdgeInsets.all(12.0)),
                 ),
+                //TODO: IMPLEMENT PAGE SWITCH ON PRESSED
                 onPressed: null, 
                 child: Text(
                   'LOGIN',
                   style: TextStyle(
                     fontFamily: 'Poppins',
                     fontSize: 20
-                    
                   ),
                 )
               )
             ]
           ),
         ),
-      ),
+      )
     );
   }
 }
