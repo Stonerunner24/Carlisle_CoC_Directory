2c0b2401d4db95faa53c0415974a25a8246e8f06
Stonerunner24 <eli.coyle1@gmail.com>
2023-07-09 15:11:56 -0400

    finished aesthetic work on login page. Logic needed for page switching and authentication.

--- carlisle_coc_directory/lib/login.dart
+++ carlisle_coc_directory/lib/login.dart
@@ -15,6 +15,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context){
     Color containerColor = convertFromHex("#35dbff");
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: Container(
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
@@ -23,97 +24,175 @@ class _LoginPageState extends State<LoginPage> {
             image: AssetImage("assets/images/Login.png"),
           )
         ),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              color: containerColor.withOpacity(0.1),
-              border: Border.all(
-                color: Colors.white.withOpacity(0.3)
-              )
-            ),
-            margin: const EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-              bottom: 16.0,
-              top: 460.0
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.only(
-                    left: 27.0,
-                    right: 27.0,
-                    top: 24.0,
-                  ),
-                  child: Text(
-                    'Carlisle Login',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Poppins',
-                      fontSize: 20,
+        child: SingleChildScrollView(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color: containerColor.withOpacity(0.1),
+                border: Border.all(
+                  color: Colors.white.withOpacity(0.3)
+                )
+              ),
+              margin: const EdgeInsets.only(
+                left: 16.0,
+                right: 16.0,
+                bottom: 16.0,
+                top: 460.0
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 24.0,
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 27.0,
-                    right: 27.0,
-                    top: 17.0
-                  ),
-                  child: Container(
-                    height: 2.0,
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.2)
-                    ),
-                  ),
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                    left: 27.0,
-                    right: 27.0,
-                    top: 24.0,
-                  ),
-                  child: Text(
-                    'Email or Username',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'Poppins',
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 27.0,
-                    right: 27.0,
-                    top: 4.0
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: 'foreverfaithful@gmail.com',
-                      hintStyle: TextStyle(
-                        color: Colors.white.withOpacity(0.5),
+                    child: Text(
+                      'Carlisle Login',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
                         fontFamily: 'Poppins',
-                        fontSize: 16,
+                        fontSize: 20,
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 17.0
+                    ),
+                    child: Container(
+                      height: 2.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2)
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 24.0,
+                    ),
+                    child: Text(
+                      'Email or Username',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 4.0
+                    ),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'foreverfaithful@gmail.com',
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                          fontFamily: 'Poppins',
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 24.0,
+                    ),
+                    child: Text(
+                      'Password',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 4.0
+                    ),
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: '********',
+                        hintStyle: TextStyle(
+                          color: Colors.white.withOpacity(0.5),
+                          fontFamily: 'Poppins',
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      left: 27.0,
+                      right: 27.0,
+                      top: 24.0,
+                      bottom: 48.0
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        TextButton(
+                          onPressed: null,
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'Poppins',
+                              fontSize: 12,
+                            )
+                          ),
+                        ),
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStatePropertyAll<Color>(Colors.white),
+                            foregroundColor: MaterialStatePropertyAll<Color>(Colors.black),
+                            shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(2.0)))
+                            ),
+                            padding: MaterialStatePropertyAll<EdgeInsets>(
+                              EdgeInsets.only(
+                                left: 29.0,
+                                right: 29.0,
+                                top: 4.0,
+                                bottom: 4.0
+                              )
+                            ),
+                          ),
+                          onPressed: null, 
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 14 
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            // child: BackdropFilter(
-            //   filter: ImageFilter.blur(
-            //     sigmaX: 5.0,
-            //     sigmaY: 5.0
-            //   ),
-            // ),
           ),
-        
         ),
       ),
     );
