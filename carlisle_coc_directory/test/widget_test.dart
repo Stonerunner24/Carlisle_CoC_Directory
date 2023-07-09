a03940b804abd5a167dc0cb27e2d487ba969bf43
Stonerunner24 <eli.coyle1@gmail.com>
2023-07-09 15:39:40 -0400

    Put the pages in a dedicated 'pages' directory. Created Home page.

--- carlisle_coc_directory/test/widget_test.dart
+++ carlisle_coc_directory/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:carlisle_coc_directory/main.dart';
+import 'package:carlisle_coc_directory/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
